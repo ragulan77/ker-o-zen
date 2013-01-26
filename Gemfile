50410903523625c3aef87e7d3ac38c3395b8b524
Fabrice Carrega <fabrice.carrega@treize37.com>
2013-01-26 10:43:30 +0100

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,19 @@
+source 'https://rubygems.org'
+
+# Core application
+gem 'rails', '3.2.11'
+
+# Content management system (CMS)
+gem 'locomotive_cms', '~> 2.0.0.rc12', :require => 'locomotive/engine'
+
+# Assets management
+group :assets do
+  gem 'sass-rails',     '~> 3.2.6'
+  gem 'coffee-rails',   '~> 3.2.2'
+  gem 'uglifier',       '>= 1.3.0'
+  gem 'compass-rails',  '~> 1.0.3'
+end
+
+group :development do
+  gem 'unicorn'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KerOZen::Application.config.secret_token = 'b5d2b90c145c7f1a62360f5d903e0c215aed807408b8424a41081f0df43fea56c9b1df6fd0a32f770a729fcb36e6d10650022ab30e190aa8fa57319f24e2ae11'
